1acf35494c903a03c5c19ec1b8c4595b5bc61fa6
MacOS <accountas@mail.ru>
2021-09-20 09:12:33 +0400

    Выравнивание
    Signed-off-by: MacOS <accountas@mail.ru>

--- src/components/templates/traderiq.js
+++ src/components/templates/traderiq.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
   container: { flex: 1 },
   header: {},
   content: {
-    flex: 9,
+    flex: 8,
     // alignContent: "center",
     // justifyContent: "center",
   },

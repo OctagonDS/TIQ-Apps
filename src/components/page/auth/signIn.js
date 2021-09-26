6ad6e7b626a6f3a3746177309e8e8dd3045d42a3
Madvender <accountas@mail.ru>
2021-09-27 01:21:51 +0400

    sss
    Signed-off-by: Madvender <accountas@mail.ru>

--- src/components/page/auth/signIn.js
+++ src/components/page/auth/signIn.js
@@ -3,12 +3,10 @@ import React from "react"
 import {
   View,
   Text,
-  ScrollView,
   StyleSheet,
-  Image,
   ImageBackground,
-  SafeAreaView,
   TouchableOpacity,
+  TextInput,
 } from "react-native"
 import { gStyle } from "../../../styles/style"
 import { ArrowLeft } from "../../atoms/arrowLeft"
@@ -18,16 +16,40 @@ const image = require("../../../assets/img/black-geo.png")
 export const SignIn = ({ navigation: { goBack } }) => {
   return (
     <ImageBackground source={image} resizeMode="cover" style={{ flex: 1 }}>
-      <View style={gStyle.main}>
+      <View style={[gStyle.main, { justifyContent: "center" }]}>
         <TouchableOpacity onPress={() => goBack()}>
           <ArrowLeft style={styles.arrow} />
         </TouchableOpacity>
-        <Text style={gStyle.title}>Авторизация</Text>
+        <Text style={styles.title}>Anmelden</Text>
       </View>
     </ImageBackground>
   )
 }
 
 export const styles = StyleSheet.create({
-  arrow: { marginLeft: "10%", marginTop: "15%" },
+  arrow: {
+    marginLeft: "8%",
+    marginTop: "15%",
+    marginBottom: "10%",
+  },
+  block: {
+    alignItems: "center",
+  },
+  wrapper: {
+    width: "70%",
+    height: 68,
+  },
+  submitTextLog: {
+    color: "#fff",
+    textAlign: "center",
+    fontFamily: "ub-medium",
+    fontSize: 24,
+  },
+  title: {
+    fontSize: 40,
+    fontFamily: "ub-medium",
+    textAlign: "center",
+    color: "#fff",
+    letterSpacing: 2,
+  },
 })

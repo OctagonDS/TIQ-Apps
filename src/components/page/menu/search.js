3161bab40ce59ce66336bd9aff34e5a3c33ad808
Madvender <accountas@mail.ru>
2021-09-29 10:36:38 +0400

    Поиск
    Signed-off-by: Madvender <accountas@mail.ru>

--- src/components/page/menu/search.js
+++ src/components/page/menu/search.js
@@ -1,13 +1,35 @@
 import React from "react"
-import { View, Text, ScrollView, StyleSheet, Platform } from "react-native"
+import { View, Text, ScrollView, StyleSheet, TextInput } from "react-native"
 import { gStyle } from "../../../styles/style"
 
 export const SearchModal = (props) => {
   return (
-    <View style={gStyle.main}>
-      <ScrollView>
-        {/* <Text style={gStyle.title}>Страница профиля</Text> */}
-      </ScrollView>
+    <View style={[gStyle.main, { alignContent: "center" }]}>
+      <View>
+        <Text style={styles.title}>Suche</Text>
+        <TextInput style={styles.input} placeholder="Was suchen wir?" />
+      </View>
+      <ScrollView></ScrollView>
     </View>
   )
 }
+
+export const styles = StyleSheet.create({
+  title: {
+    fontFamily: "ub-reg",
+    fontSize: 40,
+    color: "#545A60",
+    alignSelf: "center",
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: "#FF741F",
+    width: "80%",
+    marginLeft: "10%",
+    marginTop: "7%",
+    height: 50,
+    borderRadius: 7,
+    fontSize: 18,
+    paddingLeft: 10,
+  },
+})

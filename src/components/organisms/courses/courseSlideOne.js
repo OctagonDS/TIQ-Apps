6e647764bc2e36a174e824732bc50f1df84fc959
Madvender <accountas@mail.ru>
2021-11-13 07:02:00 +0400

    API 1
    Signed-off-by: Madvender <accountas@mail.ru>

---
+++ src/components/organisms/courses/courseSlideOne.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react'
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  Image,
+  RefreshControl,
+} from 'react-native'
+import { gStyle } from '../../../styles/style'
+import { NavigationContainer } from '@react-navigation/native'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+
+const wait = (timeout) => {
+  return new Promise((resolve) => setTimeout(resolve, timeout))
+}
+
+export const CourseSlideOne = ({ navigation }) => {
+  const [isLoading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  const [refreshing, setRefreshing] = React.useState(false)
+
+  const url = 'https://fe20295.online-server.cloud/api/v1/courses'
+
+  const getCourses = async () => {
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      const json = await response.json()
+      setData(json.data)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+  const onRefresh = React.useCallback(() => {
+    getCourses(true)
+    wait(2000).then(() => getCourses(false))
+  }, [])
+
+  useEffect(() => {
+    getCourses()
+  }, [])
+
+  const [layout, setLayout] = useState({
+    width: 0,
+    height: 0,
+  })
+
+  return (
+    // <ScrollView
+    //   style={{ flex: 1, backgroundColor: '#fff' }}
+    //   refreshControl={
+    //     <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+    //   }
+    // >
+    <View
+      style={{
+        // margin: 0,
+        marginBottom: '20%',
+        // marginTop: '10%',
+        // alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+      }}
+      onLayout={(event) => setLayout(event.nativeEvent.layout)}
+    >
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          numColumns={2}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                alignItems: 'center',
+                width: '50%',
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: '#000',
+                  margin: 10,
+                  width: 150,
+                  height: 130,
+                  borderRadius: 8,
+                  justifyContent: 'space-around',
+                }}
+              >
+                <Image
+                  style={{
+                    width: 100,
+                    height: 120,
+                    resizeMode: 'contain',
+                    alignSelf: 'center',
+                  }}
+                  source={{
+                    uri: `https://fe20295.online-server.cloud/storage/${item.image_сourses}`,
+                  }}
+                />
+              </View>
+              <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>
+                {item.title}
+              </Text>
+            </View>
+          )}
+        />
+      )}
+    </View>
+    // </ScrollView>
+  )
+}
--- src/components/page/courses.js
+++ src/components/page/courses.js
@@ -1,50 +1,107 @@
-import React from "react"
-import { View, Text, ScrollView, StyleSheet, Button } from "react-native"
-import { gStyle } from "../../styles/style"
-import { NavigationContainer } from "@react-navigation/native"
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
+import React, { useEffect, useState } from 'react'
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  Image,
+  RefreshControl,
+} from 'react-native'
+import { gStyle } from '../../styles/style'
+import { NavigationContainer } from '@react-navigation/native'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { CourseSlideOne } from '../organisms/courses/courseSlideOne'
 
-function HomeScreen({ navigation }) {
-  return (
-    <ScrollView style={{ flex: 1, backgroundColor: "#fff" }}>
-      {/* <View
-        style={{
-          marginBottom: 10,
-          marginTop: 30,
-          width: "50%",
-          marginLeft: "25%",
-        }}
-      >
-        <Button
-          title="Продукт"
-          onPress={() => navigation.navigate("Product")}
-        />
-      </View>
-      <View
-        style={{
-          marginBottom: 10,
-          marginTop: 10,
-          width: "50%",
-          marginLeft: "25%",
-        }}
-      >
-        <Button
-          title="Главная страница"
-          onPress={() => navigation.navigate("Greeting")}
-        />
-      </View> */}
-    </ScrollView>
-  )
-}
+// const wait = (timeout) => {
+//   return new Promise((resolve) => setTimeout(resolve, timeout))
+// }
+
+// function HomeScreen({ navigation }) {
+//   const [isLoading, setLoading] = useState(true)
+//   const [data, setData] = useState([])
+
+//   const [refreshing, setRefreshing] = React.useState(false)
+
+//   const url = 'https://fe20295.online-server.cloud/api/v1/courses'
+
+//   const getCourses = async () => {
+//     try {
+//       const response = await fetch(url, {
+//         method: 'GET',
+//         headers: {
+//           Accept: 'application/json',
+//           'Content-Type': 'application/json',
+//         },
+//       })
+//       const json = await response.json()
+//       setData(json.data)
+//     } catch (error) {
+//       console.error(error)
+//     } finally {
+//       setLoading(false)
+//     }
+//   }
+//   const onRefresh = React.useCallback(() => {
+//     getCourses(true)
+//     wait(2000).then(() => getCourses(false))
+//   }, [])
+
+//   useEffect(() => {
+//     getCourses()
+//   }, [])
+
+//   return (
+//     <ScrollView
+//       style={{ flex: 1, backgroundColor: '#fff' }}
+//       refreshControl={
+//         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+//       }
+//     >
+//       <View
+//         style={{
+//           margin: '2%',
+//           marginBottom: '25%',
+//           marginTop: '10%',
+//           alignItems: 'center',
+//         }}
+//       >
+//         {isLoading ? (
+//           <ActivityIndicator />
+//         ) : (
+//           <FlatList
+//             data={data}
+//             numColumns={2}
+//             keyExtractor={({ id }, index) => id}
+//             renderItem={({ item }) => (
+//               <View style={{ alignItems: 'center' }}>
+//                 <Image
+//                   style={{ width: 150, height: 210, marginRight: '3%' }}
+//                   source={{
+//                     uri: `https://fe20295.online-server.cloud/storage/${item.image_сourses}`,
+//                   }}
+//                 />
+//                 <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>
+//                   {item.title}
+//                 </Text>
+//               </View>
+//             )}
+//           />
+//         )}
+//       </View>
+//     </ScrollView>
+//   )
+// }
 
 function SettingsScreen() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#fff",
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
       }}
     >
       {/* <Text>Настройки!</Text> */}
@@ -60,25 +117,25 @@ export function Courses() {
       screenOptions={{
         tabBarIndicatorContainerStyle: {
           borderBottomWidth: 1,
-          borderBottomColor: "#D8D8D8",
+          borderBottomColor: '#D8D8D8',
         },
         tabBarStyle: {
           elevation: 0,
           shadowOpacity: 0,
         },
-        tabBarActiveTintColor: "#FF741F",
-        tabBarInactiveTintColor: "#545A60",
+        tabBarActiveTintColor: '#FF741F',
+        tabBarInactiveTintColor: '#545A60',
         tabBarIndicatorStyle: {
-          backgroundColor: "#FF741F",
+          backgroundColor: '#FF741F',
         },
         tabBarLabelStyle: {
-          fontFamily: "ub-medium",
-          textTransform: "none",
+          fontFamily: 'ub-medium',
+          textTransform: 'none',
           fontSize: 14,
         },
       }}
     >
-      <Tab.Screen name="Kostenlose Kurse" component={HomeScreen} />
+      <Tab.Screen name="Kostenlose Kurse" component={CourseSlideOne} />
       <Tab.Screen name="Bezahlte Kurse" component={SettingsScreen} />
     </Tab.Navigator>
   )

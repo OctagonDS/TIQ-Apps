88783e57eb8eb4a015ba5ef076e19f976fcf36a2
Madvender <accountas@mail.ru>
2021-11-17 12:42:49 +0400

    test
    Signed-off-by: Madvender <accountas@mail.ru>

--- App.js
+++ App.js
@@ -1,33 +1,22 @@
-import React, { useState } from "react"
-import { View } from "react-native"
-import * as Font from "expo-font"
-import AppLoading from "expo-app-loading"
-import { Navigations } from "./src/navigation/navigation"
+import React, { useState } from 'react'
+import { View } from 'react-native'
+import * as Font from 'expo-font'
+import { Navigations } from './src/navigation/navigation'
 
 const fonts = () =>
   Font.loadAsync({
-    "ub-bold": require("./src/assets/fonts/Ubuntu-Bold.ttf"),
-    "ub-bolditalic": require("./src/assets/fonts/Ubuntu-BoldItalic.ttf"),
-    "ub-italic": require("./src/assets/fonts/Ubuntu-Italic.ttf"),
-    "ub-light": require("./src/assets/fonts/Ubuntu-Light.ttf"),
-    "ub-lightitalic": require("./src/assets/fonts/Ubuntu-LightItalic.ttf"),
-    "ub-medium": require("./src/assets/fonts/Ubuntu-Medium.ttf"),
-    "ub-mediumitalic": require("./src/assets/fonts/Ubuntu-MediumItalic.ttf"),
-    "ub-reg": require("./src/assets/fonts/Ubuntu-Regular.ttf"),
+    'ub-bold': require('./src/assets/fonts/Ubuntu-Bold.ttf'),
+    'ub-bolditalic': require('./src/assets/fonts/Ubuntu-BoldItalic.ttf'),
+    'ub-italic': require('./src/assets/fonts/Ubuntu-Italic.ttf'),
+    'ub-light': require('./src/assets/fonts/Ubuntu-Light.ttf'),
+    'ub-lightitalic': require('./src/assets/fonts/Ubuntu-LightItalic.ttf'),
+    'ub-medium': require('./src/assets/fonts/Ubuntu-Medium.ttf'),
+    'ub-mediumitalic': require('./src/assets/fonts/Ubuntu-MediumItalic.ttf'),
+    'ub-reg': require('./src/assets/fonts/Ubuntu-Regular.ttf'),
   })
 
 export default function App() {
-  const [font, setFont] = useState(false)
+  const [font, setFont] = useState(true)
 
-  if (font) {
-    return <Navigations />
-  } else {
-    return (
-      <AppLoading
-        startAsync={fonts}
-        onFinish={() => setFont(true)}
-        onError={console.warn}
-      />
-    )
-  }
+  return <Navigations />
 }

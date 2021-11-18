496f0bde5b4bb83863c7cb5dfef9cf649911a075
Madvender <accountas@mail.ru>
2021-11-18 04:23:45 +0400

    Accordion
    Signed-off-by: Madvender <accountas@mail.ru>

--- src/components/page/menu/faq.js
+++ src/components/page/menu/faq.js
@@ -1,13 +1,153 @@
-import React from "react"
-import { View, Text, ScrollView, StyleSheet, Platform } from "react-native"
-import { gStyle } from "../../../styles/style"
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+} from 'react-native'
+import { gStyle } from '../../../styles/style'
+import Constants from 'expo-constants'
+import Accordion from 'react-native-collapsible/Accordion'
 
-export const FaqPage = (props) => {
-  return (
-    <View style={gStyle.main}>
-      <ScrollView>
-        {/* <Text style={gStyle.title}>Страница профиля</Text> */}
-      </ScrollView>
-    </View>
-  )
+const BACON_IPSUM =
+  'Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. '
+
+const CONTENT = [
+  {
+    title: 'First',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Second',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Third',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Fourth',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Fifth',
+    content: BACON_IPSUM,
+  },
+]
+
+export class FaqPage extends Component {
+  state = {
+    activeSections: [],
+    multipleSelect: false,
+  }
+
+  setSections = (sections) => {
+    this.setState({
+      activeSections: sections.includes(undefined) ? [] : sections,
+    })
+  }
+
+  renderHeader = (section, _, isActive) => {
+    return (
+      <View style={[styles.header, isActive ? styles.active : styles.inactive]}>
+        <Text style={styles.headerText}>{section.title}</Text>
+      </View>
+    )
+  }
+
+  renderContent(section, _, isActive) {
+    return (
+      <View
+        style={[styles.content, isActive ? styles.active : styles.inactive]}
+      >
+        <Text>{section.content}</Text>
+      </View>
+    )
+  }
+
+  render() {
+    const { multipleSelect, activeSections } = this.state
+
+    return (
+      <View style={gStyle.main}>
+        <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
+          <Text style={styles.title}>Accordion Example</Text>
+
+          <Accordion
+            activeSections={activeSections}
+            sections={CONTENT}
+            touchableComponent={TouchableOpacity}
+            expandMultiple={multipleSelect}
+            renderHeader={this.renderHeader}
+            renderContent={this.renderContent}
+            duration={400}
+            onChange={this.setSections}
+            renderAsFlatList={false}
+          />
+        </ScrollView>
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    paddingTop: Constants.statusBarHeight,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: '300',
+    marginBottom: 20,
+  },
+  header: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  content: {
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  active: {
+    backgroundColor: 'rgba(255,255,255,1)',
+  },
+  inactive: {
+    backgroundColor: 'rgba(245,252,255,1)',
+  },
+  selectors: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  selector: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  activeSelector: {
+    fontWeight: 'bold',
+  },
+  selectTitle: {
+    fontSize: 14,
+    fontWeight: '500',
+    padding: 10,
+  },
+  multipleToggle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 30,
+    alignItems: 'center',
+  },
+  multipleToggle__title: {
+    fontSize: 16,
+    marginRight: 8,
+  },
+})
